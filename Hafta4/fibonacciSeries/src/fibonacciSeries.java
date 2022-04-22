af3aaccd7641f82aa75a762e6b60e6a2249855bc
ecemnebioglu <98159237+ecemnebioglu@users.noreply.github.com>
2022-04-22 23:45:13 +0300

    Code Update

--- Hafta4/fibonacciSeries/src/fibonacciSeries.java
+++ Hafta4/fibonacciSeries/src/fibonacciSeries.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class fibonacciSeries {
     public static void main(String[] args) {
-        int n1=0, n2=1,n3,i,count=10;
+        Scanner sc = new Scanner(System.in);
+        int n1=0, n2=1,n3,i,count;
+
+        System.out.println("Fibonacci Serisinin Eleman Sayısını Giriniz : ");
+        count=sc.nextInt();
+
         System.out.print(n1+" "+n2);
         for (i=2;i<count;i++){
             n3=n1+n2;
