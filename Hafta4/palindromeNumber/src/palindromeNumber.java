0880635910d8fcab7f2fa15e41c3be0b3353c9b1
ecemnebioglu <98159237+ecemnebioglu@users.noreply.github.com>
2022-04-23 21:25:14 +0300

    First Commit

---
+++ Hafta4/palindromeNumber/src/palindromeNumber.java
@@ -0,0 +1,18 @@
+public class palindromeNumber {
+    static boolean isPalindrome(int number) {
+        int temp = number, reverseNumber = 0, lastDigit;
+        while (temp != 0) {
+            lastDigit = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(7007));;
+    }
+}
