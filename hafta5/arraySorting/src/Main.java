32a73e141a0fcd7b96a850c58b5c1be644482049
Ecem Nebioğlu <98159237+ecemnebioglu@users.noreply.github.com>
2022-05-02 02:24:25 +0300

    Update Main.java

--- hafta5/arraySorting/src/Main.java
+++ hafta5/arraySorting/src/Main.java
@@ -12,25 +12,16 @@ public class Main {
         for (int i = 0; i < n; i++) {
             System.out.print((i + 1) + ". elemanı : ");
             arr[i] = sc.nextInt();
-            for (int j=0; j<n-1;j++){
-                for(int k=(j+1);k<n;k++){
-                    if(arr[k]<arr[j]){
-                        temp= arr[j];
-                        arr[j]=arr[k];
-                        arr[k]=temp;
-                    }
-                }
-            }
         }
-//        for(int i = 0; i<n-1;i++) {
-//            for(int j=(i+1);j<n;j++){
-//                if (arr[j]<arr[i]) {
-//                    temp = arr[i];
-//                    arr[i]=arr[j];
-//                    arr[j]=temp;
-//                }
-//            }
-//        }
-        System.out.print("Sıralama : "+Arrays.toString(arr));
+       for(int i = 0; i<n-1;i++) {
+           for(int j=(i+1);j<n;j++){
+               if (arr[j]<arr[i]) {
+                   temp = arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+               }
+           }
+       }
+        System.out.print("Sıralama : " + Arrays.toString(arr));
     }
 }
